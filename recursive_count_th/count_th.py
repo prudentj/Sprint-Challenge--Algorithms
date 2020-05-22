b4cc647298b767d1603046d3be17ceb7c07ba2be
Jacob Prudent <jake.prudent@gmail.com>
2020-05-22 09:39:21 -0700

    MVP with stretch

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -1,10 +1,34 @@
 '''
 Your function should take in a single parameter (a string `word`)
-Your function should return a count of how many occurences of ***"th"*** occur within `word`. Case matters.
+Your function should return a count of how many occurences of
+ ***"th"*** occur within `word`. Case matters.
 Your function must utilize recursion. It cannot contain any loops.
 '''
+
+
 def count_th(word):
-    
-    # TBC
-    
-    pass
+    count = 0
+    # If the word is a single letter
+    if len(word) < 2:
+        return 0
+    # If the word is two letters
+    # check to see if they match
+    if len(word) == 2:
+        if word[0] == 't':
+            if word[1] == 'h':
+                return 1
+            else:
+                return 0
+        else:
+            return 0
+    # if it is bigger split it in half
+    # check if the halves contain th
+    # slide the half over one index and check again
+    if len(word) > 2:
+        count += count_th(word[:len(word)//2])
+        count += count_th(word[len(word)//2:])
+        count += count_th(word[len(word)//2-1:len(word)//2+1])
+    return count
+
+
+# print(count_th("abcthxyz"))
